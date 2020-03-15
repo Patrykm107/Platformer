cdca226396a2e1f63128da28d8b8e49e4f00036c
Patrykm107 <patrykm987@gmail.com>
2020-03-16 00:27:45 +0100

    Added player movement and terrain collisions

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	[SerializeField] private float jumpStrength = 300f;
+	[Range(0, .3f)] [SerializeField] private float movementSmooth = .05f;
+	[SerializeField] private LayerMask groundLayers;
+	[SerializeField] private Transform groundCheck;
+
+	const float onGroundRadius = .2f;
+	private bool onGround;
+
+	private Rigidbody2D rrigidbody2D;
+	private Vector3 velocity = Vector3.zero;
+
+	void Awake()
+    {
+		rrigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+	private void FixedUpdate()
+	{
+		Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, onGroundRadius, groundLayers);
+		for (int i = 0; i < colliders.Length; i++)
+		{
+			if (colliders[i].gameObject != gameObject)
+			{
+				onGround = true;
+			}
+		}
+	}
+
+	public void Move(float move, bool jump)
+	{
+		Vector3 targetVelocity = new Vector3(move * 10f, rrigidbody2D.velocity.y);
+		rrigidbody2D.velocity = Vector3.SmoothDamp(rrigidbody2D.velocity, targetVelocity, ref velocity, movementSmooth);
+
+		if(onGround && jump)
+		{
+			onGround = false;
+			rrigidbody2D.AddForce(new Vector2(0, jumpStrength));
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public PlayerController controller;
+
+    public float runSpeed = 40f;
+
+    private float horizontalMove = 0f;
+    bool jump = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        
+        if (Input.GetButton("Jump"))
+        {
+            jump = true;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove * Time.deltaTime, jump);
+        jump = false;
+    }
+}
